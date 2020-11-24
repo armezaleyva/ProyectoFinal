26c4affd2ee15b0a8d432870f7abbfc0628019f5
Armando Meza Leyva <armezaleyva@gmail.com>
2020-11-24 08:20:35 -0700

    Terminar UI en storyboard y modificar controllers

--- ProyectoFinal/Controllers/FirstViewController.swift
+++ ProyectoFinal/Controllers/FirstViewController.swift
@@ -142,17 +142,16 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
             Player(role: "supp", name: "Mia"),
         ]
         
-        
-        teams.append(Team(rank: 1, name: "KT", wins: 18, losses: 0, gameDiff: 36, streak: 18, img: "kt", players: ktPlayers, fullName: "KT Rolster", teamImage: "kti"))
-        teams.append(Team(rank: 2, name: "DWG", wins: 16, losses: 2, gameDiff: 29, streak: 10, img: "dwg", players: dwgPlayers, fullName: "DAMWON Gaming", teamImage: "dwgi"))
-        teams.append(Team(rank: 3, name: "DRX", wins: 15, losses: 3, gameDiff: 19, streak: -1, img: "drx", players: drxPlayers, fullName: "DRX", teamImage: "drxi"))
-        teams.append(Team(rank: 4, name: "GEN", wins: 14, losses: 4, gameDiff: 19, streak: 4, img: "geng", players: gengPlayers, fullName: "Gen.G", teamImage: "gengi"))
-        teams.append(Team(rank: 5, name: "T1", wins: 13, losses: 5, gameDiff: 14, streak: 1, img: "t1", players: t1Players, fullName: "T1", teamImage: "t1i"))
-        teams.append(Team(rank: 6, name: "AF", wins: 10, losses: 8, gameDiff: 1, streak: -1, img: "af", players: afPlayers, fullName: "Afreeca Freecs", teamImage: "afi"))
-        teams.append(Team(rank: 7, name: "SB", wins: 7, losses: 11, gameDiff: -8, streak: 1, img: "sb", players: sbPlayers, fullName: "Sandbox GAMING", teamImage: "sbi"))
-        teams.append(Team(rank: 8, name: "DYN", wins: 5, losses: 13, gameDiff: -12, streak: -1, img: "dyn", players: dynPlayers, fullName: "Team Dynamics", teamImage: "dyni"))
-        teams.append(Team(rank: 9, name: "HLE", wins: 2, losses: 16, gameDiff: -26, streak: 1, img: "hle", players: hlePlayers, fullName: "Hanwha Life Esports", teamImage: "hlei"))
-        teams.append(Team(rank: 10, name: "SP", wins: 1, losses: 17, gameDiff: -29, streak: -17, img: "sp", players: spPlayers, fullName: "SeolHaeOne Prince", teamImage: "spi"))
+        teams.append(Team(rank: 1, name: "KT", wins: 18, losses: 0, gameDiff: 36, streak: 18, img: "kt", players: ktPlayers, teamImage: "kti"))
+        teams.append(Team(rank: 2, name: "DWG", wins: 16, losses: 2, gameDiff: 29, streak: 10, img: "dwg", players: dwgPlayers, teamImage: "dwgi"))
+        teams.append(Team(rank: 3, name: "DRX", wins: 15, losses: 3, gameDiff: 19, streak: -1, img: "drx", players: drxPlayers, teamImage: "drxi"))
+        teams.append(Team(rank: 4, name: "GEN", wins: 14, losses: 4, gameDiff: 19, streak: 4, img: "geng", players: gengPlayers, teamImage: "gengi"))
+        teams.append(Team(rank: 5, name: "T1", wins: 13, losses: 5, gameDiff: 14, streak: 1, img: "t1", players: t1Players, teamImage: "t1i"))
+        teams.append(Team(rank: 6, name: "AF", wins: 10, losses: 8, gameDiff: 1, streak: -1, img: "af", players: afPlayers, teamImage: "afi"))
+        teams.append(Team(rank: 7, name: "SB", wins: 7, losses: 11, gameDiff: -8, streak: 1, img: "sb", players: sbPlayers, teamImage: "sbi"))
+        teams.append(Team(rank: 8, name: "DYN", wins: 5, losses: 13, gameDiff: -12, streak: -1, img: "dyn", players: dynPlayers, teamImage: "dyni"))
+        teams.append(Team(rank: 9, name: "HLE", wins: 2, losses: 16, gameDiff: -26, streak: 1, img: "hle", players: hlePlayers, teamImage: "hlei"))
+        teams.append(Team(rank: 10, name: "SP", wins: 1, losses: 17, gameDiff: -29, streak: -17, img: "sp", players: spPlayers, teamImage: "spi"))
     }
 
     override func didReceiveMemoryWarning() {
--- ProyectoFinal/Controllers/SecondViewController.swift
+++ ProyectoFinal/Controllers/SecondViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var selectedPlayer: Player?
+    var players: [Player] = []
+    
+    let reuseId = "playerCell"
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseId) as! MVPPlayerTableViewCell
+        let player = players[indexPath.row]
+        
+        cell.lblRank.text = "\(player.mvpRank!)"
+        cell.imgTeam.image = UIImage(named: player.team!)
+        cell.lblName.text = player.name
+        cell.lblPoints.text = "\(player.mvpPoints!)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlayer = players[indexPath.row]
+        performSegue(withIdentifier: "playerDetailsSegue", sender: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        players.append(Player(role: "bot", name: "Aiming", team: "kt", mvpPoints: 1600, mvpRank: 1, img: "aiming", fullName: "Kim \"Aiming\" Ha-ram"))
+        players.append(Player(role: "mid", name: "Bdd", team: "geng", mvpPoints: 1300, mvpRank: 2, img: "bdd", fullName: "Gwak \"Bdd\" Bo-seong"))
+        players.append(Player(role: "mid", name: "Showmaker", team: "dwg", mvpPoints: 1100, mvpRank: 3, img: "showmaker", fullName: "Heo \"Showmaker\" Su"))
+        players.append(Player(role: "top", name: "Canna", team: "t1", mvpPoints: 1000, mvpRank: 4, img: "canna", fullName: "Kim \"Canna\" Chang-dong"))
+        players.append(Player(role: "jg", name: "Canyon", team: "dwg", mvpPoints: 1000, mvpRank: 4, img: "canyon", fullName: "Kim \"Canyon\" Geon-bu"))
+        players.append(Player(role: "mid", name: "Chovy", team: "drx", mvpPoints: 1000, mvpRank: 4, img: "chovy", fullName: "Jeong \"Chovy\" Ji-hoon"))
+        players.append(Player(role: "top", name: "Rich", team: "dyn", mvpPoints: 900, mvpRank: 7, img: "rich", fullName: "Lee \"Rich\" Jae-won"))
+        players.append(Player(role: "supp", name: "Keria", team: "drx", mvpPoints: 800, mvpRank: 8, img: "keria", fullName: "Ryu \"Keria\" Min-seok"))
+        players.append(Player(role: "bot", name: "Ruler", team: "geng", mvpPoints: 700, mvpRank: 9, img: "ruler", fullName: "Park \"Ruler\" Jae-hyuk"))
+        players.append(Player(role: "supp", name: "BeryL", team: "dwg", mvpPoints: 600, mvpRank: 10, img: "beryl", fullName: "Cho \"BeryL\" Geon-hee"))
+        players.append(Player(role: "supp", name: "Doran", team: "drx", mvpPoints: 600, mvpRank: 10, img: "doran", fullName: "Choi \"Doran\" Hyeon-joon"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "playerDetailsSegue" {
+            let destination = segue.destination as! PlayerDetailsViewController
+            destination.player = selectedPlayer!
+        }
     }
 
-
 }
-
