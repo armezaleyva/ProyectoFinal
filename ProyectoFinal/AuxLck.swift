046f21c7f5d2d1ea63b35fb57a6f28711e6fb59d
Armando Meza Leyva <armezaleyva@gmail.com>
2020-11-25 09:58:11 -0700

    Fix 2

--- ProyectoFinal/AuxLck.swift
+++ ProyectoFinal/AuxLck.swift
@@ -11,45 +11,46 @@ import Foundation
 class AuxLck {
     static func getFullTeamName(shortTeamName: String) -> String {
         switch shortTeamName {
-            case "kt":
-                return "KT Rolster"
-            case "dwg":
-                return "DAMWON Gaming"
-            case "drx":
-                return "DRX"
-            case "geng":
-                return "Gen.G"
-            case "t1":
-                return "T1"
-            case "af":
-                return "Afreeca Freecs"
-            case "sb":
-                return "Sandbox GAMING"
-            case "dyn":
-                return "Team Dynamics"
-            case "hle":
-                return "Hanwha Life Esports"
-            case "sp":
-                return "SeolHaeOne Prince"
-            default:
-                return ""
+        case "kt":
+            return "KT Rolster"
+        case "dwg":
+            return "DAMWON Gaming"
+        case "drx":
+            return "DRX"
+        case "geng":
+            return "Gen.G"
+        case "t1":
+            return "T1"
+        case "af":
+            return "Afreeca Freecs"
+        case "sb":
+            return "Sandbox GAMING"
+        case "dyn":
+            return "Team Dynamics"
+        case "hle":
+            return "Hanwha Life Esports"
+        case "sp":
+            return "SeolHaeOne Prince"
+        default:
+            return ""
         }
     }
     
     static func getRoleName(shortRoleName: String) -> String {
         switch shortRoleName {
-            case "top":
-                return "TOP"
-            case "jg":
-                return "JUNGLE"
-            case "mid":
-                return "MID"
-            case "bot":
-                return "BOT"
-            case "supp":
-                return "SUPP"
+        case "top":
+            return "TOP"
+        case "jg":
+            return "JUNGLE"
+        case "mid":
+            return "MID"
+        case "bot":
+            return "BOT"
+        case "supp":
+            return "SUPP"
         default:
             return ""
         }
     }
 }
+
